f9d885ff69b70ee6ec3b1b831bbdcdedb8739fbf
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-11-30 22:21:34 +0900

    feat(CredentialHelper): UserPasswordRequired

--- WindowsSudoDaemon/CredentialHelper.cs
+++ WindowsSudoDaemon/CredentialHelper.cs
@@ -32,19 +32,25 @@ namespace WindowsSudo
 
         public static bool UserExists(string username, string domain)
         {
+            if (domain == null)
+                return UserExists(username);
+
             using (PrincipalContext context = new PrincipalContext(ContextType.Domain, domain))
             {
-                return UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username) != null;
+                return UserPrincipal.FindByIdentity(context, username) != null;
             }
         }
 
         public static bool ValidateAccount(string name, string password)
         {
-            ContextType ct = ContextType.Machine;
+            if (password == null &&
+                UserExists(name) &&
+                !UserPasswordRequired(name))
+                return true;
 
             try
             {
-                using (PrincipalContext pc = new PrincipalContext(ct))
+                using (PrincipalContext pc = new PrincipalContext(ContextType.Machine))
                 {
                     return pc.ValidateCredentials(name, password);
                 }
@@ -55,8 +61,22 @@ namespace WindowsSudo
             }
         }
 
-        public static bool ValidateAccount(string name, string password, string domain)
+        public static bool ValidateAccount(string name, string password, string domain, bool exception=false)
         {
+            if (password == null)
+            {
+                if (!DomainExists(domain))
+                    throw new Exceptions.DomainNotFoundException("Could not find domain " + domain);
+                if (!UserExists(name, domain))
+                    throw new Exceptions.UserNotFoundException("Could not find user " + domain);
+                if (UserPasswordRequired(name, domain))
+                    throw new Exceptions.PasswordRequiredException("Password required for user " + name);
+                return true;
+            }
+            else if (!UserExists(name, domain))
+                throw new Exceptions.UserNotFoundException("Could not find user " + name);
+
+
             try
             {
                 using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domain))
@@ -68,6 +88,60 @@ namespace WindowsSudo
             {
                 return false;
             }
+        }
+
+        public static bool UserPasswordRequired(string username, string domain)
+        {
+            if (domain == null)
+                return UserPasswordRequired(username);
+
+            try
+            {
+                using (PrincipalContext context = new PrincipalContext(ContextType.Domain, domain))
+                {
+                    UserPrincipal user = UserPrincipal.FindByIdentity(context, username);
+
+                    if (user == null)
+                        return true;
+                    else
+                        user.ChangePassword("", "");
+                    return false;
+                }
+            }
+            catch (PasswordException)
+            {
+                return true;
+            }
+            catch (PrincipalOperationException)
+            {
+                return true;
+            }
+        }
+
+        public static bool UserPasswordRequired(string username)
+        {
+            try
+            {
+                using (PrincipalContext context = new PrincipalContext(ContextType.Machine))
+                {
+                    UserPrincipal user = UserPrincipal.FindByIdentity(context, username);
+
+                    if (null == user)
+                        return true;
+                    else
+                        user.ChangePassword("", "");
+                    return false;
+                }
+            }
+            catch (PasswordException)
+            {
+                return true;
+            }
+            catch (PrincipalOperationException)
+            {
+                return true;
+            }
+
         }
 
         public static bool ACAvailable()
@@ -100,6 +174,15 @@ namespace WindowsSudo
                 public BadPasswordException(string message, Exception innerException) : base(message, innerException) { }
                 protected BadPasswordException(SerializationInfo info, StreamingContext context) : base(info, context) { }
             }
+
+            [Serializable()]
+            public class PasswordRequiredException : Exception
+            {
+                public PasswordRequiredException(string message) : base(message) { }
+                public PasswordRequiredException(string message, Exception innerException) : base(message, innerException) { }
+                protected PasswordRequiredException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+            }
+
 
 
             [Serializable()]
